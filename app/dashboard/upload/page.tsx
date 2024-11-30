76adf2d831a3ce65262275e82505cf4ec5d709ce
Rachael Oldham-Knott <reok@Rachaels-MBP.lan>
2024-11-30 22:07:39 +0000

    added react-dropzone, setup flie uploaded component

---
+++ app/dashboard/upload/page.tsx
@@ -0,0 +1,11 @@
+import FileUploader
+ from "@/components/FileUploader"
+function Upload() {
+  return (
+    <div>
+      <FileUploader />
+    </div>
+  )
+}
+
+export default Upload
---
+++ components/FileUploader.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+import {
+    CheckCircleIcon,
+    CircleArrowDown,
+    HammerIcon,
+    RocketIcon,
+    SaveIcon
+} from 'lucide-react'
+
+
+function FileUploader() {
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        
+    }, [])
+    const { getRootProps, getInputProps, isDragActive, isFocused, isDragAccept } = useDropzone({ onDrop })
+    return (
+        <div className='flex flex-col gap-4 items-center max-w-7xl mx-auto'>
+            <div {...getRootProps()}
+                className={`p-10 border-teal-600 border-2 border-dashed mt-10 w-[90%] text-teal-600 rounded-lg h-96 flex items-center justify-center ${isFocused || isDragAccept ? 'bg-teal-300' : 'bg-teal-100'}`}
+            >
+                <input {...getInputProps()} />
+                <div className='flex flex-col justify-center items-center'>
+                    {
+                        isDragActive ? (
+                            <>
+                                <RocketIcon className="h-16 w-16 animate-ping" />
+                                <p>Drop the files here ...</p>
+                            </>
+                        ) : (
+                            <>
+                                <CircleArrowDown className="h-16 w-16 animate-bounce" />
+                                <p>Drag 'n' drop some files here, or click to select files</p>
+                            </>
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FileUploader
