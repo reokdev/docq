eb20bdca84dd98ee0984131d0cc05642e68f3583
Rachael Oldham-Knott <reokwebdev@gmail.com>
2024-12-04 18:12:00 +0000

    refactor: move Firebase service account to env variable and remove debug logs

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,13 +1,15 @@
 import { initializeApp, getApps, getApp, App, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceKey = require("@/service_key.json");
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '', 'base64').toString()
+);
 
 let app: App;
 
 if (getApps().length === 0) {
     app = initializeApp({
-        credential: cert(serviceKey),
+        credential: cert(serviceAccount),
     });
 } else {
     app = getApp();
